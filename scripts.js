972625de5d2a7eb231929fc790fe727f9c4df288
Krishna Jones <krishna123jones@gmail.com>
2023-11-03 08:15:14 -0500

    completed scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    var fm = document.querySelector('.newsletter button');
+    const eml = document.getElementById('email');
+    var mess = document.querySelector('.message');
+
+    fm.addEventListener('click', function(event) 
+    {
+        event.preventDefault();
+
+
+        if (eml.value === '') 
+        {
+            mess.textContent = 'Please enter a valid email address.';
+        } 
+        else 
+        {
+            mess.textContent = `Thank you! Your email address ${eml.value} has been added to our mailing list!`;
+        }
+    });
+});
